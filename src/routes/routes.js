0e88bfc0e738e20fcd87ef167623253f98be4d9e
TienHunter <tienkbtnhp@gmail.com>
2022-07-02 20:06:56 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/routes/routes.js
@@ -0,0 +1,19 @@
+//
+import config from '~/config';
+//layout
+import { HeaderOnly } from '~/layouts';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+
+//public route
+const publicRoutes = [
+   { path: config.routes.home, element: Home },
+   { path: config.routes.following, element: Following },
+   { path: config.routes.profile, element: Profile },
+   { path: config.routes.upload, element: Upload, layout: HeaderOnly },
+];
+const privateRoutes = [];
+export { publicRoutes, privateRoutes };
