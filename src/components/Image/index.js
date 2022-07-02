ee44007aa0cdf6c34fddc6344e8606d0045f59ac
TienHunter <tienkbtnhp@gmail.com>
2022-07-02 17:54:57 +0700

    fix: Using forwardRef with proptypes and eslint

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,34 +1,44 @@
 import PropTypes from 'prop-types';
 import { useState, forwardRef } from 'react';
 import images from '~/assets/image';
-function Image(
-   { src, alt, className, fallback: customFallback = images.noImage, ...props },
-   ref,
-) {
-   // console.log(images.noImage);
-   const [fallback, setFallback] = useState('');
-   const hanleError = () => {
-      setFallback(customFallback);
-   };
-   // console.log(src);
-   // console.log(fallback);
-   // console.log(customFallback);
-   return (
-      <img
-         src={fallback || src}
-         alt={alt}
-         {...props}
-         ref={ref}
-         onError={hanleError}
-         className={className}
-         style={{ overflow: 'hidden' }}
-      />
-   );
-}
+const Image = forwardRef(
+   (
+      {
+         src,
+         alt,
+         className,
+         fallback: customFallback = images.noImage,
+         ...props
+      },
+      ref,
+   ) => {
+      // console.log(images.noImage);
+      const [fallback, setFallback] = useState('');
+      const hanleError = () => {
+         setFallback(customFallback);
+      };
+      // console.log(src);
+      // console.log(fallback);
+      // console.log(customFallback);
+      return (
+         <img
+            src={fallback || src}
+            alt={alt}
+            {...props}
+            ref={ref}
+            onError={hanleError}
+            className={className}
+            style={{ overflow: 'hidden' }}
+         />
+      );
+   },
+);
 Image.propTypes = {
    src: PropTypes.string,
    alt: PropTypes.string,
    className: PropTypes.string,
    fallback: PropTypes.string,
 };
-export default forwardRef(Image);
+// Image.displayName = 'Image';
+// export default forwardRef(Image);
+export default Image;
